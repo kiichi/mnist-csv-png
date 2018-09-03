37ff85675ba7bd6e5872e916312b04f8d6e2934b
kiichi <kiichi.takeuchi@objectgraph.com>
2018-09-03 12:45:29 -0400

    using curl
    e.g. Mac user

--- process.sh
+++ process.sh
@@ -3,10 +3,10 @@
 mkdir -p train
 mkdir -p test
 
-wget https://pjreddie.com/media/files/train-images-idx3-ubyte.gz
-wget https://pjreddie.com/media/files/train-labels-idx1-ubyte.gz
-wget https://pjreddie.com/media/files/t10k-images-idx3-ubyte.gz
-wget https://pjreddie.com/media/files/t10k-labels-idx1-ubyte.gz
+curl https://pjreddie.com/media/files/train-images-idx3-ubyte.gz -o train-images-idx3-ubyte.gz
+curl https://pjreddie.com/media/files/train-labels-idx1-ubyte.gz -o train-labels-idx1-ubyte.gz 
+curl https://pjreddie.com/media/files/t10k-images-idx3-ubyte.gz -o t10k-images-idx3-ubyte.gz 
+curl https://pjreddie.com/media/files/t10k-labels-idx1-ubyte.gz -o t10k-labels-idx1-ubyte.gz
 
 gunzip train-images-idx3-ubyte.gz
 gunzip train-labels-idx1-ubyte.gz
@@ -14,4 +14,3 @@ gunzip t10k-images-idx3-ubyte.gz
 gunzip t10k-labels-idx1-ubyte.gz
 
 python process_mnist.py
-
